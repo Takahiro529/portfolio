1c13777b3841f73e5c19e4d8e17fa2a4b6160a7a
Takahiro Ono <taka3mail529@gmail.com>
2020-01-22 14:30:07 +0900

    修正

--- mail.php
+++ mail.php
@@ -1,50 +1,57 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    // POSTでのアクセスでない場合
-    $name = '';
-    $email = '';
-    $subject = '';
-    $message = '';
-    $err_msg = '';
-    $complete_msg = '';
-
+ 
+// パラメータ取得
+$request_param = $_POST;
+ 
+// お問い合わせ日時
+$request_datetime = date("Y年m月d日 H時i分s秒");
+ 
+//自動返信メール
+$mailto = $request_param['email'];
+$to = 'taka3mail529@gmail.com';
+$mailfrom = "From:taka3mail529@gmail.com";
+$subject = "お問い合わせ有難うございます。";
+ 
+$content = "";
+$content .= $request_param['name']. "様\r\n";
+$content .= "お問い合わせ有難うございます。\r\n";
+$content .= "お問い合わせ内容は下記通りでございます。\r\n";
+$content .= "=================================\r\n";
+$content .= "お名前	      " . htmlspecialchars($request_param['name'])."\r\n";
+$content .= "メールアドレス   " . htmlspecialchars($request_param['email'])."\r\n";
+$content .= "内容   " . htmlspecialchars($request_param['message'])."\r\n";
+$content .= "お問い合わせ日時   " . $request_datetime."\r\n";
+$content .= "=================================\r\n";
+ 
+//管理者確認用メール
+$subject2 = "お問い合わせがありました。";
+$content2 = "";
+$content2 .= "お問い合わせがありました。\r\n";
+$content2 .= "お問い合わせ内容は下記通りです。\r\n";
+$content2 .= "=================================\r\n";
+$content2 .= "お名前	      " . htmlspecialchars($request_param['name'])."\r\n";
+$content2 .= "メールアドレス   " . htmlspecialchars($request_param['email'])."\r\n";
+$content2 .= "内容   " . htmlspecialchars($request_param['message'])."\r\n";
+$content2 .= "お問い合わせ日時   " . $request_datetime."\r\n";
+$content2 .= "================================="."\r\n";
+ 
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+//mail 送信
+if($request_param['token'] === '1234567'){
+if(mb_send_mail($to, $subject2, $content2, $mailfrom)){
+    mb_send_mail($mailto,$subject,$content,$mailfrom);
+    ?>
+    <script>
+        window.location = 'メールを送信した後に表示されるページのurl';
+    </script>
+    <?php
 } else {
-    // フォームがサブミットされた場合（POST処理）
-    // 入力された値を取得する
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    // エラーメッセージ・完了メッセージの用意
-    $err_msg = '';
-    $complete_msg = '';
-
-    // 空チェック
-    if ($name == '' || $email == '' || $subject == '' || $message == '') {
-        $err_msg = '全ての項目を入力してください。';
-    }
-
-    // エラーなし（全ての項目が入力されている）
-    if ($err_msg == '') {
-        $to = 'taka3mail529@gmail.com'; // 管理者のメールアドレスなど送信先を指定
-        $headers = "From: " . $email . "\r\n";
-
-        // 本文の最後に名前を追加
-        $message .= "\r\n\r\n" . $name;
-
-        // メール送信
-        mb_send_mail($to, $subject, $message, $headers);
-
-        // 完了メッセージ
-        $complete_msg = '送信されました！';
-
-        // 全てクリア
-        $name = '';
-        $email = '';
-        $subject = '';
-        $message = '';
-    }
-
+    header('Content-Type: text/html; charset=UTF-8');
+  echo "メールの送信に失敗しました";
+};
+} else {
+echo "メールの送信に失敗しました（トークンエラー）";
 }
+ 
 ?>
